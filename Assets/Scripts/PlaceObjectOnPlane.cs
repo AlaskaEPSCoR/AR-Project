810b093cc8ae42997592fd63bbf7fea9cb127087
itsonlynaomi <55763271+itsonlynaomi@users.noreply.github.com>
2023-10-11 09:40:17 -0800

    making new script

--- Assets/Scripts/PlaceObjectOnPlane.cs
+++ Assets/Scripts/PlaceObjectOnPlane.cs
@@ -2,38 +2,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using UnityEngine.InputSystem.EnhancedTouch;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 
 [RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
 
 public class PlaceObject : MonoBehaviour
 {
-    public GameObject prefab;
-    public ARRaycastManager aRRaycastManager;
-    public ARPlaneManager aRPlaneManager; 
-    public List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    [SerializeField]
+    private GameObject prefab;
 
-    public void Awake(){
+    private ARRaycastManager aRRaycastManager;
+    private ARPlaneManager aRPlaneManager; 
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake(){
         aRRaycastManager = GetComponent<ARRaycastManager>();
         aRPlaneManager = GetComponent<ARPlaneManager>();
 
     }
-    public void onEnable(){
+    private void onEnable(){
         TouchSimulation.Enable();
         EnhancedTouchSupport.Enable();
-        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
     }
-    public void onDisable(){
+    private void onDisable(){
         TouchSimulation.Disable();
         EnhancedTouchSupport.Disable();
-        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
     }
-    public void FingerDown(Finger finger) {
+    private void FingerDown(Finger finger) {
         if (finger.index != 0) return;
+
         if(aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon)){
             foreach(ARRaycastHit hit in hits){
                 Pose pose = hit.pose;
-                GameObject obj = Instantiate(prefab);
+                GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
             }
         }
     }
